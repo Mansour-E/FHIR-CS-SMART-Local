e77054b321afe830c4f42ed168b2f4c8f534fe47
Mansour Emami <mansuurem@gmail.com>
2026-02-03 17:54:19 +0100

    Url server setup

--- src/Program.cs
+++ src/Program.cs
@@ -2,11 +2,31 @@
 
 namespace smart_local
 {
-    class Program
+    /// <summary>
+    /// Main program
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        private const string _defaultFhirServerUrl = "https://launch.smarthealthit.org/v/r4/sim/WzIsIiIsIjFjYjUxMTU3LTgwODMtNDEwZi04N2QxLTA3YTk0NjI5MjIyYSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMSwiIl0/fhir";
+        
+        
+        /// <summary>
+        /// Program to access a SMART FHIR Server with a local webserver for redirection
+        /// </summary>
+        /// <param name="fhirServerUrl">FHIR R4 endpoint URL</param>
+        /// <returns></returns>
+        static int Main(
+            string fhirServerUrl
+            )
         {
-            Console.WriteLine("Hello World!");
+            if (string.IsNullOrEmpty(fhirServerUrl))
+            {
+                fhirServerUrl = _defaultFhirServerUrl;
+            }
+
+            System.Console.WriteLine($"FHIR Server: {fhirServerUrl}");
+            
+            return 0;
         }
     }
 }

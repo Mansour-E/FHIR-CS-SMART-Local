4dff352f0d9318878a88c1e42f885528899edfed
Mansour Emami <mansuurem@gmail.com>
2026-02-22 16:57:03 +0100

    setup die default server

--- src/Program.cs
+++ src/Program.cs
@@ -2,22 +2,21 @@
 
 namespace smart_local
 {
+
     /// <summary>
-    /// Main program
+    /// Main Program
     /// </summary>
     public static class Program
     {
         private const string _defaultFhirServerUrl = "https://launch.smarthealthit.org/v/r4/sim/WzIsIiIsIjFjYjUxMTU3LTgwODMtNDEwZi04N2QxLTA3YTk0NjI5MjIyYSIsIkFVVE8iLDAsMCwwLCIiLCIiLCIiLCIiLCIiLCIiLCIiLDAsMSwiIl0/fhir";
-        
-        
+
+
         /// <summary>
         /// Program to access a SMART FHIR Server with a local webserver for redirection
         /// </summary>
-        /// <param name="fhirServerUrl">FHIR R4 endpoint URL</param>
+        /// <param name="fhirServerUrl"></param>
         /// <returns></returns>
-        static int Main(
-            string fhirServerUrl
-            )
+        static int Main(string fhirServerUrl)
         {
             if (string.IsNullOrEmpty(fhirServerUrl))
             {
@@ -26,21 +25,7 @@ namespace smart_local
 
             System.Console.WriteLine($"FHIR Server: {fhirServerUrl}");
 
-            Hl7.Fhir.Rest.FhirClient fhirClient = new Hl7.Fhir.Rest.FhirClient(fhirServerUrl);
-
-            if(!FhirUtils.GetSmartUrls(fhirClient, out string authorizeUrl , out string tokenUrl))
-            {
-                System.Console.WriteLine($"Failed to discover SMART URLs");
-                return -1;
-            }
-
-            System.Console.WriteLine($"   FHIR Server: {fhirServerUrl}");
-            System.Console.WriteLine($" Authorize URL: {authorizeUrl}");
-            System.Console.WriteLine($"     Token URL: {tokenUrl}");
-            
-            return 0;
+           return 0;
         }
     }
 }
-
-
